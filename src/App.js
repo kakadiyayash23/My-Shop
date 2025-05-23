943dfab62938a290996e287f83ffcec6006db62d
kakadiyayash23 <yashkakadiya.dev@gmail.com>
2025-05-23 13:44:16 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+import Checkout from './Checkout';
+
+export default function App() {
+  return (
+    <div>
+      <Checkout />
+    </div>
+  );
+}
